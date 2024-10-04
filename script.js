64771691a8d3652f3bc9c6f581f49ecfa8d5cabf
Timothy Otholt <timotholt@gmail.com>
2024-10-03 22:49:20 -0700

    Changed welcome message!

--- script.js
+++ script.js
@@ -1650,7 +1650,7 @@ initGameState();
 window.alert(
     `ASCII Dungeon!\n\n` +
     `An unfinished adventure game.\n\n` +
-    `The first step is to make your character.\n`
+    `The first step is to look over your character and spend your skill points.\n`
 );
 
 // Open character sheet
